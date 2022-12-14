a135faa0341c084ec03bfbf1906a385c4f193702
francesca <francesca.ps.1601@gmail.com>
2022-12-14 14:17:25 +0100

    prova rami

--- src/main/java/com/ispwproject/adoptme/HelloApplication.java
+++ src/main/java/com/ispwproject/adoptme/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ShelterAddPet.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("AdoptMe");
+//        stage.setTitle("AdoptMe");
         stage.setScene(scene);
         stage.show();
     }
