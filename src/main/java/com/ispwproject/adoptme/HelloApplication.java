83854d3f5449a02278e9795ae95423312693aaf8
francesca <francesca.ps.1601@gmail.com>
2022-12-17 19:36:48 +0100

    Aggiunta seconda pagina dell'AddPetForm

--- src/main/java/com/ispwproject/adoptme/HelloApplication.java
+++ src/main/java/com/ispwproject/adoptme/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     private static Stage stage;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ShelterAddPet.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         HelloApplication.stage = stage;
         stage.setTitle("AdoptMe");
