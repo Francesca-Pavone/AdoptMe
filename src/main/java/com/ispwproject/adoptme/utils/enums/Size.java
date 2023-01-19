2254410008e7c3fb289bdc91fb9b70f28a6a9c33
federicacantelmi <federicacantelmii@gmail.com>
2023-01-19 14:52:35 +0100

    Prova size

--- src/main/java/com/ispwproject/adoptme/utils/enums/Size.java
+++ src/main/java/com/ispwproject/adoptme/utils/enums/Size.java
@@ -1,5 +1,10 @@
 package com.ispwproject.adoptme.utils.enums;
 
 public enum Size {
-    Small, Medium, Large, ExtraLarge
+    Small (1), Medium(2), Large(3), ExtraLarge(4);
+
+    int i;
+    Size(int i) {
+        this.i = i;
+    }
 }
