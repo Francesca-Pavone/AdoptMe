c9796cb646816d6d5680bda4ea0c2d50389b6d7c
federicacantelmi <federicacantelmii@gmail.com>
2023-01-28 19:29:46 +0100

    Aggiunto UserSettings

---
+++ src/main/java/com/ispwproject/adoptme/controller/graficcontroller/cli/CLIUserSettingsController.java
@@ -0,0 +1,26 @@
+package com.ispwproject.adoptme.controller.graficcontroller.cli;
+
+import com.ispwproject.adoptme.engineering.session.Session;
+import com.ispwproject.adoptme.view.cli.CLILoginView;
+import com.ispwproject.adoptme.view.cli.CLIUserSettingsView;
+
+public class CLIUserSettingsController {
+    private final static String MODIFY_NAME= "1";
+    private final static String MODIFY_SURNAME= "2";
+    private final static String MODIFY_EMAIL= "3";
+    private final static String MODIFY_PASSWORD= "4";
+    private final static String LOGOUT= "5";
+
+    public void executeCommand(String inputLine) throws Exception {
+        switch (inputLine) {
+            case MODIFY_NAME, MODIFY_SURNAME, MODIFY_EMAIL, MODIFY_PASSWORD -> {
+                System.out.println("Functionality not developed yet.");
+                CLIUserSettingsView.run();
+            }
+            case LOGOUT -> {
+                Session.getCurrentSession().closeSession();
+                CLILoginView.run();
+            }
+        }
+    }
+}
