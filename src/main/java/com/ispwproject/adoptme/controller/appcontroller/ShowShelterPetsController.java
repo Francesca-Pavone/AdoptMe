501833a6384067e7247b177778f321742bd60379
Francesca Pavone <francesca.ps.1601@gmail.com>
2023-02-06 16:48:03 +0100

    .

--- src/main/java/com/ispwproject/adoptme/controller/appcontroller/ShowShelterPetsController.java
+++ src/main/java/com/ispwproject/adoptme/controller/appcontroller/ShowShelterPetsController.java
@@ -22,11 +22,9 @@ public class ShowShelterPetsController {
     public ShowShelterPetsController(){}
 
     public ShelterBean getShelter(String shelterName) throws Exception {
-        ShelterBean shelterBean = null;
         int shelterId = ShelterDAO.retrieveIdByShelterName(shelterName);
         ShelterModel shelterModel = ShelterDAO.retrieveShelterById(shelterId);
-        shelterBean = new ShelterBean(shelterModel.getId(), shelterModel.getShelterName(), shelterModel.getPhoneNumber(), shelterModel.getAddress(), shelterModel.getCity(), shelterModel.getWebSite(), shelterModel.getAccountInfo().getEmail());
-        return shelterBean;
+        return new ShelterBean(shelterModel.getId(), shelterModel.getShelterName(), shelterModel.getPhoneNumber(), shelterModel.getAddress(), shelterModel.getCity(), shelterModel.getWebSite(), shelterModel.getEmail());
     }
 
     public void getPetList(Observer observer) throws NoPetsFoundException {
@@ -38,43 +36,5 @@ public class ShowShelterPetsController {
             // Errore durante l'apertura della connessione
             se.printStackTrace();
         }
-
-        List<PetBean> petBeanList = new ArrayList<>();
-
-        for (PetModel petModel : shelterPetsList.getPetList()) {
-            PetBean petBean = new PetBean();
-            petBean.setPetId(petModel.getPetId());
-            petBean.setShelterId(petModel.getShelter().getId());
-            petBean.setPetImage(petModel.getPetImage());
-            petBean.setName(petModel.getName());
-            petBean.setType(petModel.getType());
-            petBean.setYearOfBirth(petModel.getYearOfBirth());
-            petBean.setMonthOfBirth(petModel.getMonthOfBirth());
-            petBean.setDayOfBirth(petModel.getDayOfBirth());
-            petBean.setAge(petModel.getAge());
-            petBean.setGender(petModel.getGender());
-            petBean.setCoatLenght(petModel.getCoatLenght());
-            petBean.setVaccinated(petModel.isVaccinated());
-            petBean.setMicrochipped(petModel.isMicrochipped());
-            petBean.setDewormed(petModel.isDewormed());
-            petBean.setSterilized(petModel.isSterilized());
-            petBean.setDisability(petModel.isDisability());
-            petBean.setDisabilityType(petModel.getDisabilityType());
-
-            PetCompatibility petCompatibility = petModel.getPetCompatibility();
-            petBean.setMaleDog(petCompatibility.isMaleDog());
-            petBean.setFemaleDog(petCompatibility.isFemaleDog());
-            petBean.setMaleCat(petCompatibility.isMaleCat());
-            petBean.setFemaleCat(petCompatibility.isFemaleCat());
-            petBean.setChildren(petCompatibility.isChildren());
-            petBean.setElders(petCompatibility.isElders());
-            petBean.setApartmentNoGarden(petCompatibility.isApartmentNoGarden());
-            petBean.setApartmentNoTerrace(petCompatibility.isApartmentNoTerrace());
-            petBean.setSleepOutside(petCompatibility.isSleepOutside());
-            petBean.setFirstExperience(petCompatibility.isFirstExperience());
-            petBean.setHoursAlone(petCompatibility.getHoursAlone());
-            petBeanList.add(petBean);
-        }
-
     }
 }
