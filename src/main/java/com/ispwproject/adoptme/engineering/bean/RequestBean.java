59aa1ad9b076aa36d77c3548ff42b07957699a34
Francesca Pavone <francesca.ps.1601@gmail.com>
2023-02-11 13:16:08 +0100

    sistemazione espressione regolare data

--- src/main/java/com/ispwproject/adoptme/engineering/bean/RequestBean.java
+++ src/main/java/com/ispwproject/adoptme/engineering/bean/RequestBean.java
@@ -113,8 +113,11 @@ public class RequestBean extends Subject {
     }
 
     public void setDate(String date) throws DateFormatException {
-        String dateRegex = "^(((0[1-9]|[12]\\d|3[01])-(0[13578]|1[02])|(0[1-9]|[12]\\d|30)-(0[469]|11)|(0[1-9]|1\\d|2[0-8])-02)-\\d{4})$";
-        if (!Pattern.compile(dateRegex).matcher(date).matches())
+        String dateRegex = "((0[1-9]|[12]\\d|3[01])-(0[13578]|1[02])|(0[1-9]|[12]\\d|30)-(0[469]|11))-\\d{4}";
+        String dateRegex2 = "(0[1-9]|1\\d|2[0-8])-02-\\d{4}";
+        String dateRegex3 = "29-02-(\\d{2}(0[48]|[2468][048]|[13579][26])|([02468][048]|[13579][26])00)";
+
+        if (!Pattern.compile(dateRegex).matcher(date).matches() && !Pattern.compile(dateRegex2).matcher(date).matches() && !Pattern.compile(dateRegex3).matcher(date).matches())
             throw new DateFormatException(date);
         this.date = date;
     }
